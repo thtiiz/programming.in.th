b58d064179399f6d051e20428bf0ff619c7e6639
thtiiz <napon-na123@hotmail.com>
2018-10-19 16:14:18 +0700

    Efficiencygit add 1129.cpp !

---
+++ 1129.cpp
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int f[10000];
+int main() {
+    int n, k, i, j, cur, realk;
+    scanf("%d %d", &n, &k);
+    for(i=0; i<n; i++) f[i]=i+1;
+    f[n]=1; // present for cycle
+    int count=0, x=1;
+    k--; // นับเพิ่มอีก k-1 ครั้ง
+    cur=0; //init !!! case realk=0
+    for(i=0; i<n; i++) {
+        realk=k%(n-i);          // i: 0 1 2 ...n-1
+        for(j=0; j<realk; j++){ //x->|1|2|3|...|n|
+            cur=x;
+            x = f[x]; //ex. x==1 -> x=f[1]=2;
+        }
+        printf("%d\n", x);
+        f[cur]=f[x];
+        x=f[x];
+    }
+}
